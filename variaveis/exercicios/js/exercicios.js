dc2eeb6719b45a0f086516d284eb5224c753584a
rednerdoficial <anapaulacirilo.santos@gmail.com>
2021-04-12 15:16:08 -0300

    exercicios de variáveis

---
+++ variaveis/exercicios/js/exercicios.js
@@ -0,0 +1,21 @@
+/*let varA = 'A'; //B
+let varB = 'B'; //C
+let varC = 'C'; //A
+
+varA2 = varA;
+varA = varB;
+varB = varC;
+varC = varA2;
+console.log(varA, varB, varC);
+*/
+
+// fazendo o exercicio com JS
+
+
+let varA = 'A'; //B
+let varB = 'B'; //C
+let varC = 'C'; //A
+
+[varA, varB, varC] = [varB, varC, varA];
+
+console.log(varA, varB, varC);
