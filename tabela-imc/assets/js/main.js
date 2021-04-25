654b364cb6d8faf7c533e14d3658a786f4dfed37
rednerdoficial <anapaulacirilo.santos@gmail.com>
2021-04-25 20:12:48 -0300

    set h1 color

--- tabela-imc/assets/js/main.js
+++ tabela-imc/assets/js/main.js
@@ -37,8 +37,8 @@ function getNivelImc(imc) {
 		'Obsesidade Grau 3',
 	];
 
-	if (imc >= 39.9) return nivel[5];
-	if (imc >= 34.9) return nivel[4];
+		if (imc >= 39.9) return nivel[5];
+		if (imc >= 34.9) return nivel[4];
 	if (imc >= 29.9) return nivel[3];
 	if (imc >= 24.5) return nivel[2];
 	if (imc >= 18.5) return nivel[1];
